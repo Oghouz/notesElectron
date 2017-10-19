c4212cd8b69252a9cfc6f9443981b72676772d6b
Maimaiti Maerdan <maimaiti@pharmaland.fr>
2017-10-19 12:39:43 +0200

    file action(save, open, saveAs, copy, paste ...), notificaiton, style

--- app.js
+++ app.js
@@ -5,13 +5,22 @@ const fs = require('fs');
 var content = $('#content');
 content.focus();
 
+var saveAction = false;
+
+const WindowsToaster = require('node-notifier').WindowsToaster;
+var notifier = new WindowsToaster({
+    withFallback: false, // Fallback to Growl or Balloons? 
+    customPath: void 0 // Relative path if you want to use your fork of toast.exe 
+});
+
 const template = [
     {
         label: 'Fichier',
         submenu: [
         	{
         		label: 'Nouveau',
-        		accelerator: 'Ctrl+N'
+        		accelerator: 'Ctrl+N',
+        		click: () => { newFile() }
         	},
         	{
                 label: 'Ouvrir...',
@@ -23,6 +32,21 @@ const template = [
                 accelerator: 'Ctrl+S',
                 click: () => { saveFile() }
             }, 
+            {
+            	label: 'Enregistrer sous...',
+            	click: () => { saveAsFile() }
+            },
+            {
+            	type: 'separator'
+            },
+            {
+            	label: 'Imprimer...',
+            	accelerator: 'Ctrl+P',
+            	click: () => {}
+            },
+            {
+            	type: 'separator'
+            },
             {
                 label: 'Quitter',
                 accelerator: 'Ctrl+Q',
@@ -39,11 +63,13 @@ const template = [
             }, 
             {
                 label: 'Copier',
-                accelerator: 'Ctrl+C'
+                accelerator: 'Ctrl+C',
+                click: () => { document.execCommand('copy') }
             }, 
             {
                 label: 'Coller',
-                accelerator: 'Ctrl+V'
+                accelerator: 'Ctrl+V',
+                click: () => { document.execCommand('paste') }
             }
         ]
     }, 
@@ -84,21 +110,39 @@ const template = [
     				}
     			]
     		},
-    		
+    		{
+    			label: 'Taille de texte',
+    			submenu: [
+    				{
+    					label: 'Petit',
+    					click: () => { setFontSize(12) }
+    				},
+    				{
+    					label: 'Moyenne',
+    					click: () => { setFontSize(16) }
+    				},
+    				{
+    					label: 'Grande',
+    					click: () => { setFontSize(21) }
+    				},
+    			]
+    		}
     	]
     }
 ];
 
-function quitApp()
+/**
+ * @return {[type]} [description]
+ */
+function newFile()
 {
-	remote.dialog.showMessageBox({
-	    type: 'warning',
-	    title: 'Attention !',
-	    message: 'Vous-voulez vraiment quitter cette application?',
-	    buttons: ['OK', 'Cancel']
-	});
+	saveAction = false;
+	content.val('');
 }
 
+/**
+ * @return {[type]} [description]
+ */
 function openFile()
 {
 	remote.dialog.showOpenDialog(remote.getCurrentWindow(),{
@@ -109,6 +153,7 @@ function openFile()
 	} ,(filename) => {
 		if(filename) {
 			var path = String(filename).replace(/\//g, 'ForwardSlash');
+			saveAction = path;
 			fs.readFile(path, 'utf-8', (err, data) => {
 				content.val(data);
 			})
@@ -116,38 +161,142 @@ function openFile()
 	})
 }
 
+/**
+ * @return {[type]} [description]
+ */
 function saveFile()
 {
-	remote.dialog.showSaveDialog(remote.getCurrentWindow(), {
-		title: 'Enregistre votre fichier',
-		filters: [
-			{name: 'Fichier de texte', extensions: ['txt']},
-			{name: 'Tous les fichier', extensions: ['*']}
-		]
-	},
-	(file) => {
-
-			fs.appendFile(file, content.val(), function (err) {
-				if (err) {
-					console.log(err);
+	if (saveAction) {
+		fs.writeFile(saveAction, content.val(), function (err) {
+			if (err) {
+				console.log(err);
+			}
+		})
+	} else {
+		remote.dialog.showSaveDialog(remote.getCurrentWindow(), {
+			title: 'Enregistre votre fichier',
+			filters: [
+				{name: 'Fichier de texte', extensions: ['txt']},
+				{name: 'Tous les fichier', extensions: ['*']}
+			]
+		},
+		(file) => {
+			if (file) {
+				saveAction = file;
+				fs.appendFile(file, content.val(), function (err) {
+					if (err) {
+						console.log(err);
+					}
+				})
+				notif(`Vous avez enregistre sous ${file}`);
 				}
-			})
-			alert(`Vous avez enregistre sous ${file}`);
+			}
+		)
+	}
+}
+
+/**
+ * @return {[type]} [description]
+ */
+function saveAsFile()
+{
+	remote.dialog.showSaveDialog(remote.getCurrentWindow(), {
+			title: 'Enregistre votre fichier',
+			filters: [
+				{name: 'Fichier de texte', extensions: ['txt']},
+				{name: 'Tous les fichier', extensions: ['*']}
+			]
+		},
+		(file) => {
+			if (file) {
+				saveAction = file;
+				fs.writeFile(file, content.val(), function (err) {
+					if (err) {
+						console.log(err);
+					}
+				})
+				notif(`Vous avez enregistre sous ${file}`);
+			}
 		}
 	)
 }
 
+/**
+ * Quit application
+ * @return {[type]} [description]
+ */
+function quitApp()
+{
+	remote.dialog.showMessageBox({
+	    type: 'warning',
+	    title: 'Attention !',
+	    message: 'Vous-voulez vraiment quitter cette application?',
+	    buttons: ['OK', 'Cancel']
+	}, function (result) {
+		if (!result) {
+			app.quit();
+		}
+	});
+}
+
+/**
+ * Set content bacground color
+ * @param {[type]} color [description]
+ */
 function setContentBg(color)
 {
 	content.attr("style", "background-color: " + color + ";")
 }
 
+/**
+ * Set content text color
+ * @param {[type]} color [description]
+ */
 function SetContentFontColor(color)
 {
 	content.css("color", color);
+}
+
+/**
+ * Set content font size
+ * @param {[type]} size [description]
+ */
+function setFontSize(size)
+{
+	content.css("font-size", size);
+}
+
+/**
+ * Notification
+ * @param  {[type]} message [description]
+ * @return {[type]}         [description]
+ */
+function notif(message)
+{
+	notifier.notify({
+	    title: "Bloc-notes Electron",
+	    message: message,
+	    icon: "assets/note.png", 
+	    sound: true,
+	    wait: false,
+	}, function(error, response) {
+	    console.log(response);
+	});
 }
 
 const menu = remote.Menu.buildFromTemplate(template);
 Menu.setApplicationMenu(menu);
 
 
+function setFrameTitle()
+{
+	if (saveAction) {
+		$(document).ready(function() {
+		    document.title = 'Note-bloc Electron ('+saveAction+')';
+		});
+	}
+}
+
+
+
+ 
